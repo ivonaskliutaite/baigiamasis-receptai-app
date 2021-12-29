4dba8c8eeb104832144a74237e871ac815fb420c
Ivona Skliutaite <ivonaskliutaite@gmail.com>
2021-12-29 22:51:42 +0200

    That's all

--- js/kontaktine_forma.js
+++ js/kontaktine_forma.js
@@ -9,16 +9,16 @@ $(document).ready(function() {
         if (name == '' || email == '' || contact == '') { //jeigu yra tusti laukeliai, prasome juuos uzpildyti
             alert("Uzpildykite trukstamus laukelius");
         } else {
-            $.post("kontaktine_forma.phpdfewf", { //loadin'a data is esamos db
+            $.post("kontaktine_forma.php", { //loadin'a data is esamos db
                 vardas: name,
                 elpastas: email,
                 zinute: message,
                 kontaktas: contact
             }, function(data) {
                 $("#returnmessage").append(data); // pridejimas informcijos i duomenu baze (append (data))
-                // if (data == "Artimiausiu laiku su Jumis susisieksime.") {
-                //     $("#form")[0].reset(); // jei viskas uzpildyta teisingai, laukeliai pasidaro tusti
-                // }
+                 if (data == "Artimiausiu laiku su Jumis susisieksime.") {
+                    $("#form")[0].reset(); // jei viskas uzpildyta teisingai, laukeliai pasidaro tusti
+                 }
                 alert("Artimiausiu laiku su Jumis susisieksime.");
             }).fail(function (){
                 alert("Nepavyko issiusti.");
